a11567aa65ac9046eaa20a3754333bfb5c4e3fbb
Tan Cher Shen <chershen88@hotmail.com>
2017-05-02 14:29:38 +0800

    Add Logout functionality

--- app/src/main/java/com/tancs/dapp/BaseActivity.java
+++ app/src/main/java/com/tancs/dapp/BaseActivity.java
@@ -1,10 +1,24 @@
 package com.tancs.dapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import utils.VolleySingleton;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -24,6 +38,7 @@ public class BaseActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_logout:
+                requestLogout();
                 return true;
 
             case R.id.action_settings:
@@ -33,5 +48,63 @@ public class BaseActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    private void requestLogout() {
+
+        String user_id;
+        String token;
+
+        SharedPreferences prefs = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
+        user_id = prefs.getString("user_id", "");
+        token = prefs.getString("mobile_token", "");
+
+        String url = "http://192.168.1.101:3000/api/v1/logout/" + user_id + "/" + token;
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.DELETE, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(BaseActivity.this, "Logout succesfully", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        NetworkResponse networkResponse = error.networkResponse;
+                        if (networkResponse != null && networkResponse.data != null) {
+                            String jsonError = new String(networkResponse.data);
+                            JSONObject errors;
+
+                            try {
+                                errors = new JSONObject(jsonError);
+                                // Print Error!
+                                //Toast.makeText(BaseActivity.this, errors.getString("errors"), Toast.LENGTH_LONG).show();
+                            }
+                            catch(JSONException e) {
+                                //Toast.makeText(BaseActivity.this, "Unable to parse response data !", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            //Toast.makeText(BaseActivity.this, "Unable to reach authentication server", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        VolleySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+
+        // Empty the Shared Preferences
+        SharedPreferences.Editor editor = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE).edit();
+        editor.putString("email", "");
+        editor.putString("mobile_token", "");
+        editor.putString("user_id", "");
+        editor.apply();
+
+        Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
